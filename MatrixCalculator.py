b8ce10eafd3eae9dd0ad63ba5b45f5f35a9983fc
demchenkoalexe <demchenko_alexey@mail.ru>
2019-05-07 00:08:23 +0700

    Added Matrix's add. MatrixCalculator creation test successes.

--- MatrixCalculator.py
+++ MatrixCalculator.py
@@ -1,30 +1,4 @@
-import copy
-
-def readFile(name):
-	#ЧТЕНИЕ МАТРИЦ и ОПЕРАЦИИ НАД НИМИ ИЗ ФАЙЛА
-	f = open(name, 'r')
-	firstString = True
-	matrixRead = []
-	A = []
-	for line in f:
-		if (firstString):
-			firstString = False
-			operation = line[0]
-			continue
-
-		if (operation == '+'):
-			if (line == '\n'):
-				if (not A):
-					A = copy.copy(matrixRead)
-					matrixRead = []
-			else: 
-				m = line.rstrip().split(' ')
-				m = [int(item) for item in m]
-				matrixRead.append(m)
-	
-	B = copy.copy(matrixRead)
-	f.close()
-	return operation, A, B
+from MatrixRead import readFile
 
 class MatrixCalculator():
 	def __init__(self):
