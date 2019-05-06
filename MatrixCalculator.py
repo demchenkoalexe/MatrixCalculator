6c4223639c5f6038fe2d6e6ab8919fcbaefe71c7
demchenkoalexe <demchenko_alexey@mail.ru>
2019-05-07 00:05:16 +0700

    Added Matrix's add. MatrixCalculator creation test successes.

--- MatrixCalculator.py
+++ MatrixCalculator.py
@@ -1,5 +1,31 @@
 import copy
 
+def readFile(name):
+	#ЧТЕНИЕ МАТРИЦ и ОПЕРАЦИИ НАД НИМИ ИЗ ФАЙЛА
+	f = open(name, 'r')
+	firstString = True
+	matrixRead = []
+	A = []
+	for line in f:
+		if (firstString):
+			firstString = False
+			operation = line[0]
+			continue
+
+		if (operation == '+'):
+			if (line == '\n'):
+				if (not A):
+					A = copy.copy(matrixRead)
+					matrixRead = []
+			else: 
+				m = line.rstrip().split(' ')
+				m = [int(item) for item in m]
+				matrixRead.append(m)
+	
+	B = copy.copy(matrixRead)
+	f.close()
+	return operation, A, B
+
 class MatrixCalculator():
 	def __init__(self):
 		pass
@@ -26,29 +52,7 @@ class MatrixCalculator():
 def main():
 	mc = MatrixCalculator()
 
-	#ЧТЕНИЕ МАТРИЦ и ОПЕРАЦИИ НАД НИМИ ИЗ ФАЙЛА
-	f = open('input.txt', 'r')
-	firstString = True
-	matrixRead = []
-	A = []
-	for line in f:
-		if (firstString):
-			firstString = False
-			operation = line[0]
-			continue
-
-		if (operation == '+'):
-			if (line == '\n'):
-				if (not A):
-					A = copy.copy(matrixRead)
-					matrixRead = []
-			else: 
-				m = line.rstrip().split(' ')
-				m = [int(item) for item in m]
-				matrixRead.append(m)
-	
-	B = copy.copy(matrixRead)
-	f.close()
+	operation, A, B = readFile('input.txt')
 
 	if ( operation == '+' ):
 		C = mc.addTwoMatrix(A, B)
